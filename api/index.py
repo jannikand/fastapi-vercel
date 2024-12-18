8a817541ea1bcad75d00b0c264225e79392110cd
Jannik Anderegg <jannik.anderegg@gmail.com>
2024-12-18 12:51:24 +0100

    v9

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
-app.add_middleware(CORSMiddleware, allow_origins=['https://fastapi-vercel-seven-rose.vercel.app'], 
+app.add_middleware(CORSMiddleware, allow_origins=['*'], 
                     allow_credentials=True,
                     allow_methods=["*"], 
                     allow_headers=["*"])
